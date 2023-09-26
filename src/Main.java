0793d9ff52b811d3b81675bb32a1468b6648eabf
Veronika Vishvenkova <nika.vishvenkova@gmail.com>
2023-09-26 17:02:47 +0300

    Add HelloWorld

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello Katya and Veronika!");
+        System.out.println("Hello world2!");
+
 
     }
 }
